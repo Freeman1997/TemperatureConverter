fbca1ee74a3a68b2b01b9ead3c8056c50b51ff83
Md Ishaque <ishaqrabbu97@gmail.com>
2021-12-18 23:56:18 +0600

    first commit

---
+++ MainConverter.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { converter, toCelsius, toFahrenheit } from './lib/converter'
+import TempConverter from './TempConverter'
+import "./converter.css"
+
+export default function MainConverter() {
+    const [tempValue, setTempValue] = useState('')
+    const [scaled, setScaled] = useState('c')
+
+    const onChangeHandler = (e, scale) => {
+        setTempValue(e.target.value)
+        setScaled(scale)
+    }
+
+    const celsius = scaled === 'f' ? converter(tempValue, toCelsius) : tempValue
+    const fahrenheit = scaled === 'c' ? converter(tempValue, toFahrenheit) : tempValue
+
+    return (
+        <>  
+            <h1 className="convertTitle">Temperature Converter</h1>
+            <TempConverter scale="c" onChangeHandler={onChangeHandler} temp={celsius} />
+            <TempConverter
+                scale="f"
+                onChangeHandler={onChangeHandler}
+                temp={fahrenheit}
+            />
+            
+        </>
+    )
+}
---
+++ TempConverter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function TempConverter({ scale, onChangeHandler, temp }) {
+    const parameter = { c: 'Celsius', f: 'Fahrenheit' }
+    return (
+        <>
+        <fieldset className="convert">
+        <legend>Enter temperature below in {parameter[scale]}:</legend>
+        <input
+          type="text"
+          className=""
+          value={temp}
+          onChange={(e) => onChangeHandler(e, scale)}
+        />
+      </fieldset>
+            
+        </>
+    )
+}
